4c23f16c7745b9a89bff50b47fe3ff3d87e707b2
ANURADHA THOTAKURA <anuradhathotakura@ANURADHAs-MacBook-Pro.local>
2025-04-07 19:24:55 +0530

    Initial commit.

---
+++ core/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import Question, Answer
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ['title', 'description']
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content']
---
+++ core/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('signup/', views.signup_view, name='signup_view'),
+    path('login/', views.login_view, name='login_view'),
+    path('logout/', views.logout_view, name='logout_view'),
+    path('ask/', views.post_question, name='post_question'),
+    path('question/<int:question_id>/', views.question_detail, name='question_detail'),
+    path('like/<int:answer_id>/', views.like_answer, name='like_answer'),
+]
